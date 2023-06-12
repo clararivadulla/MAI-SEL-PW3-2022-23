667a9ae118de24a1fd913580917bbd3b0b50b988
John Kelly Villota Pismag <john.villota@marktel.com>
2023-06-12 17:54:01 +0200

    Weighted method is added to Adaptation.py The ui.py is modified to allow weighted adaptation method, and the visualization of the suggestion is added.

--- source/main.py
+++ source/main.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 import pandas as pd
 from preprocessing import travel_dataset_xls_preprocessing
+from Adaptation import null_adaptation, weighted_adaptation
 
 #root = '/Users/clararivadulla/Repositories/SEL-PW3'
 # root = os.path.dirname(os.getcwd())
@@ -15,12 +16,21 @@ CB = pd.read_csv(f"{data_folder}/travel.csv")
 
 # new_case = pd.Series([ "Bathing", 2499, 2, "Egypt", "Car", 14.0, "April", "TwoStars", "Hotel White House, Egypt"],
 #                      index=[ "holiday-type", "price", "num-persons", "region", "transportation", "duration", "season", "accomodation", "hotel"])
-new_case = pd.Series([ "City", 1000, 4, "Egypt", "Car", 14.0, "April", "TwoStars", "Hotel White House, Egypt"],
-                     index=[ "holiday-type", "price", "num-persons", "region", "transportation", "duration", "season", "accomodation", "hotel"])
+new_case = pd.Series([ 2000, 4, "Egypt", "Car", 14.0, "April", "TwoStars"],
+                     index=[ "price", "num-persons", "region", "transportation", "duration", "season", "accomodation"])
 # Retrieve the most similar case
 start_time = time.time()
-most_similar_cases, distances = retrieve(CB, new_case, data_folder)
+most_similar_cases, distances = retrieve(CB, new_case, data_folder, (len(CB.index)/10))
+null_adapted_case = null_adaptation(new_case, CB.loc[most_similar_cases[0]])
+weighted_adaptation_case = weighted_adaptation(new_case, CB.loc[most_similar_cases])
 end_time = time.time()
+
+print("------ Most similar case ------")
 print(CB.loc[most_similar_cases[0]])
+print("----- Null adapted case -----")
+print(null_adapted_case)
+print("----- Weight adapted case -----")z
+print(weighted_adaptation_case)
+print("------ ----------------- ------")
 print("Time taken:", end_time - start_time, "seconds")
 print("distance:", distances[0])
