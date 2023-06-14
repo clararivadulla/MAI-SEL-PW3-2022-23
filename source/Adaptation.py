49ce371dcc842953d30072b92d2855ec81d07084
John Kelly Villota Pismag <john.villota@marktel.com>
2023-06-14 11:58:11 +0200

    For range values, the average is taken as suggestion in case there is not a given parameter.

--- source/Adaptation.py
+++ source/Adaptation.py
@@ -1,4 +1,6 @@
+import json
 import pandas as pd
+import os
 
 indexes=[ "holiday-type", "price", "num-persons", "region", "transportation", "duration", "season", "accomodation", "hotel"]
 
@@ -15,6 +17,17 @@ def null_adaptation(new_case, most_similar_case):
 
 
 def weighted_adaptation(new_case, most_similar_cases):
+    # Get back the attributes description
+    attr_dict = '\{\}'
+    file_name = os.getcwd() + "/data/attr_info.json"
+    print(file_name)
+    # Check if the min_max file exists, if not create one
+    if not os.path.exists(file_name):
+        print("File json file doesn't exist")
+    else:
+        with open(file_name, 'r') as f:
+            attr_dict = json.load(f)
+
     adapted_case = []
 
     ### Rules
@@ -31,6 +44,9 @@ def weighted_adaptation(new_case, most_similar_cases):
         if index in new_case and new_case[index] is not None :
             adapted_case.append(new_case[index])
         else:
-            adapted_case.append(most_similar_cases[index].value_counts().idxmax())
+            if attr_dict.get(index)is not None and attr_dict.get(index)["type"] == "range": # for quantitative attributes
+                adapted_case.append(most_similar_cases[index].mean())
+            else:
+                adapted_case.append(most_similar_cases[index].value_counts().idxmax())
 
     return pd.Series(adapted_case, index=indexes)
